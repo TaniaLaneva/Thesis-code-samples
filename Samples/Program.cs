13250218466b6c7a5953f6563185c45b216cbf5a
Tania Laneva <tulanewa@gmail.com>
2020-04-15 20:19:07 +0300

    Add files via upload

--- Samples/Program.cs
+++ Samples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using Samples.AdHocApproach;
 using Samples.CleanCodeApproach;
 
@@ -8,7 +9,14 @@ namespace Samples
   {
     static void Main(string[] args)
     {
-      var perimeter=new Perimeter();
+      Console.WriteLine("Hello World!");
+      PrintAdHocResults();
+      PrintCleanCodeResults();
+    }
+
+    private static void PrintAdHocResults()
+    {
+      var perimeter = new Perimeter();
       var display = perimeter.CalculatePerimeter(new Tuple<double, double, double>(1, 1, 1),
         new Tuple<double, double, double>(1, 1, 5), new Tuple<double, double, double>(1, 1, 3));
       if (display != null)
@@ -20,10 +28,11 @@ namespace Samples
       var point2 = new Point3D(1, 1, 1);
       var point3 = new Point3D(3, 3, 3);
       Console.WriteLine($"{point1.Equals(point2)} and {point1.Equals(point3)}");
-      Console.WriteLine("Hello World!");
+    }
 
-      ITriangleValidator validator = new TriangleValidator();
-      var perimeterCalculator = new TrianglePerimeterCalculator(validator);
+    private static void PrintCleanCodeResults()
+    {
+      var perimeterCalculator = new TrianglePerimeterCalculator(new TriangleValidator());
       try
       {
         var result = perimeterCalculator.Calculate(new Point3D(1, 1, 1), new Point3D(1, 1, 1), new Point3D(3, 3, 3));
