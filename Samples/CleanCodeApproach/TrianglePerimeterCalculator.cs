1a8f0ffa59d724c5dcaea5a8fd730535df4b75bb
Tania Laneva <tulanewa@gmail.com>
2020-04-15 20:36:30 +0300

    Add files via upload

--- Samples/CleanCodeApproach/TrianglePerimeterCalculator.cs
+++ Samples/CleanCodeApproach/TrianglePerimeterCalculator.cs
@@ -2,9 +2,6 @@
 {
   public class TrianglePerimeterCalculator
   {
-    private Point3D _point1;
-    private Point3D _point2;
-    private Point3D _point3;
     private readonly ITriangleValidator _triangleValidator;
 
     public TrianglePerimeterCalculator(ITriangleValidator triangleValidator)
@@ -15,21 +12,13 @@
     public double Calculate(Point3D point1, Point3D point2, Point3D point3)
     {
       _triangleValidator.CheckIfTriangleCanBeBuilt(point1, point2, point3);
-      AssignPoints(point1, point2, point3);
-      return CalculateTrianglePerimeter();
+      return CalculateTrianglePerimeter(point1, point2, point3);
     }
 
-    private double CalculateTrianglePerimeter()
+    private double CalculateTrianglePerimeter(Point3D point1, Point3D point2, Point3D point3)
     {
-      return _point1.GetDistanceToPoint(_point2) + _point1.GetDistanceToPoint(_point3) +
-             _point2.GetDistanceToPoint(_point3);
-    }
-
-    private void AssignPoints(Point3D point1, Point3D point2, Point3D point3)
-    {
-      _point1 = point1;
-      _point2 = point2;
-      _point3 = point3;
+      return point1.GetDistanceToPoint(point2) + point1.GetDistanceToPoint(point3) +
+             point2.GetDistanceToPoint(point3);
     }
   }
 }
