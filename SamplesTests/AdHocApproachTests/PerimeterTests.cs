fa5bbe569620f4a590ae09de4f1bb87aa971be26
Tania Laneva <tulanewa@gmail.com>
2020-04-15 20:21:27 +0300

    Add files via upload

---
+++ SamplesTests/AdHocApproachTests/PerimeterTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.AdHocApproachTests
+{
+  [TestClass]
+  class PerimeterTests
+  {
+    [TestMethod]
+    public void Test_Method_Sample()
+    {
+      Assert.IsTrue(true);
+    }
+  }
+}
---
+++ SamplesTests/CleanCodeApproachTests/Point3DTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.CleanCodeApproachTests
+{
+  [TestClass]
+  class Point3DTest
+  {
+  }
+}
