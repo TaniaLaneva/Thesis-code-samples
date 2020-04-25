a0cd4094ed2ba9bac21862bc14a804d4840f820e
Balázs Bónis <balazs.bonis@greenfinch.ie>
2020-04-25 11:17:49 +0100

    [AdHoc] - Added 4 preliminary tests
    There might be a bug in the Perimeter class, Given_Colinear_Points_Return_Null should pass, those 3 points are on the same line therefore they do not form a triangle

--- SamplesTests/AdHocApproachTests/PerimeterTests.cs
+++ SamplesTests/AdHocApproachTests/PerimeterTests.cs
@@ -2,16 +2,54 @@
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Samples.AdHocApproach;
 
 namespace UnitTests.AdHocApproachTests
 {
-  [TestClass]
-  class PerimeterTests
-  {
-    [TestMethod]
-    public void Test_Method_Sample()
+    [TestClass]
+    public class PerimeterTests
     {
-      Assert.IsTrue(true);
+        private Perimeter _sut;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _sut = new Perimeter();
+        }
+
+        [TestMethod]
+        public void Given_Equilateral_Triangle_Return_Perimeter()
+        {
+            // each side is Sqrt(2) long
+            var result = _sut.CalculatePerimeter(Tuple.Create(1d,0d,0d), Tuple.Create(0d,1d,0d), Tuple.Create(0d,0d,1d));
+            var expected = Math.Sqrt(2) * 3;
+            Assert.IsNotNull(result);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Given_Random_Valid_Triangle_Return_Perimeter()
+        {
+            var r = new Random();
+            // each side is Sqrt(2) long
+            var result = _sut.CalculatePerimeter(Tuple.Create(r.NextDouble(), r.NextDouble(), 0d), 
+                Tuple.Create(0d, r.NextDouble(), r.NextDouble()), 
+                Tuple.Create(r.NextDouble(), 0d, r.NextDouble()));
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Given_Two_Points_That_Are_The_Same_Return_Null()
+        {
+            var result = _sut.CalculatePerimeter(Tuple.Create(1d, 0d, 0d), Tuple.Create(1d, 0d, 0d), Tuple.Create(0d, 0d, 1d));
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void Given_Colinear_Points_Return_Null()
+        {
+            var result = _sut.CalculatePerimeter(Tuple.Create(1d, 0d, 0d), Tuple.Create(2d, 0d, 0d), Tuple.Create(-1d, 0d, 0d));
+            Assert.IsNull(result);
+        }
     }
-  }
 }
